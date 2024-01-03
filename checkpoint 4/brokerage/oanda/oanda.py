c263b0cadc86b34146d31a7e37a05b4ec64cce73
Amaar11 <101276120+Amaar11@users.noreply.github.com>
2024-01-03 20:46:29 +0100

    Update oanda.py

--- checkpoint 4/brokerage/oanda/oanda.py
+++ checkpoint 4/brokerage/oanda/oanda.py
@@ -2,8 +2,8 @@ from brokerage.oanda.TradeClient import TradeCliemt
 
 class Oanda():
 
-    def __init__(self, auth_confih={}):
-        self.trade_client = TradeCliemt(auth_config=auth_config)
+    def __init__(self, auth_config={}):
+        self.trade_client = TradeClient(auth_config=auth_config)
 
     def get_service_client(self):
         pass
